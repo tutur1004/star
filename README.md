a811934a487ea01ee27fc99db537d34bd00e799e
tutur1004 <arthur.barthelme78@gmail.com>
2020-06-05 12:35:53 +0200

    - Récupération du milieu - Fonctions pour print les différentes parties de l'étoile (Head,BodyHead/Down,Down)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # star
 
 Test technique - Programme MSc Pro - EPITECH
+'java -jar star.jar'
--- src/main/java/fr/milekat/star/Main.java
+++ src/main/java/fr/milekat/star/Main.java
@@ -6,6 +6,7 @@ import fr.milekat.star.utils.tools;
 import java.util.Scanner;
 
 public class Main {
+    private static final String DISPLAYCHAR = "*";
 
     public static void main(String[] args) {
         boolean continuer = true;
@@ -13,17 +14,16 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             log("Définissez la taille de l'étoile:");
             String input = scanner.nextLine();
-            int size = 0;
             if (tools.isNumeric(input)) {
-                size = Math.round(Float.parseFloat(input));
-                log(size + "");
+                int size = Math.round(Float.parseFloat(input));
+                if (size>=1) {
+                    printPatern(size);
+                } else {
+                    log("Merci de choisir un nombre entier suppérieur ou égual à 1.");
+                }
             } else {
-                log("Merci de choisir un nombre entier suppérieur à 0.");
+                log("Merci de choisir un nombre entier suppérieur ou égual à 1.");
             }
-
-
-
-
             log("Voulez vous continuer ? (Oui/Non)");
             String reponse = scanner.nextLine();
             if (reponse.equalsIgnoreCase("non")) continuer = false;
@@ -33,5 +33,37 @@ public class Main {
 
     public static void log(String msg) {
         System.out.println("[" + Date.setDateNow() + "] " + msg);
+    }
+
+    private static void printPatern(int n) {
+        int middle = getMiddle(n);
+
+
+    }
+
+    private static void printSpaces(int nbSpaces) {
+        for (int space = 1; space < nbSpaces; space++) {
+            System.out.print(" ");
+        }
+    }
+
+    private static int getMiddle(int n) {
+        return Math.round(n*2.8f)+1;
+    }
+
+    private static void printHead(int n) {
+
+    }
+
+    private static void printBodyHead(int n) {
+
+    }
+
+    private static void printBodyDown(int n) {
+
+    }
+
+    private static void printDown(int n) {
+
     }
 }
