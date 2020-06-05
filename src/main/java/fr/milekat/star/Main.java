15b7db8511f94130d703a1cafc25c431051928a9
tutur1004 <arthur.barthelme78@gmail.com>
2020-06-05 11:12:31 +0200

    Ajouts: - Fonction pour recommencer directement. - Vérification que l'input est numérique, et l'arrondit en int.

--- src/main/java/fr/milekat/star/Main.java
+++ src/main/java/fr/milekat/star/Main.java
@@ -1,19 +1,37 @@
 package fr.milekat.star;
 
 import fr.milekat.star.utils.Date;
+import fr.milekat.star.utils.tools;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner myInput = new Scanner(System.in);
-        log("Définissez la taille de l'étoile:");
-        int size = myInput.nextInt();
-        
+        boolean continuer = true;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            log("Définissez la taille de l'étoile:");
+            String input = scanner.nextLine();
+            int size = 0;
+            if (tools.isNumeric(input)) {
+                size = Math.round(Float.parseFloat(input));
+                log(size + "");
+            } else {
+                log("Merci de choisir un nombre entier suppérieur à 0.");
+            }
+
+
+
+
+            log("Voulez vous continuer ? (Oui/Non)");
+            String reponse = scanner.nextLine();
+            if (reponse.equalsIgnoreCase("non")) continuer = false;
+        } while (continuer);
+        log("À bientôt");
     }
 
     public static void log(String msg) {
-        System.out.print("[" + Date.setDateNow() + "] " + msg);
+        System.out.println("[" + Date.setDateNow() + "] " + msg);
     }
 }
---
+++ src/main/java/fr/milekat/star/utils/tools.java
@@ -0,0 +1,12 @@
+package fr.milekat.star.utils;
+
+public class tools {
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+}
